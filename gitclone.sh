9550adc7ea9a4005f0e819407dc4b6a6e7643c6a
Hans Georg Schaathun <georg@schaathun.net>
2025-10-01 08:30:33 +0200

    Created a stub for testing CAPQuiz

--- gitclone.sh
+++ gitclone.sh
@@ -1,5 +1,6 @@
 
 git clone -b MOODLE_500_STABLE https://github.com/moodle/moodle.git
+( cd moodle/mod && git clone https://github.com/KQMATH/moodle-mod_capquiz.git capquiz )
 ( cd moodle/question/type && git clone https://github.com/trampgeek/moodle-qtype_coderunner.git coderunner )
 
 ( cd moodle/question/behaviour/ && git clone https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner.git adaptive_adapted_for_coderunner )
--- moodle2docker/README.md
+++
@@ -1,6 +0,0 @@
-
-Apparently, cron does not work inside docker containers.
-See https://stackoverflow.com/questions/37458287/how-to-run-a-cron-job-inside-a-docker-container
-
-The problem is that cron may die, and will not restart, and this problem has been observed
-in practice.
