840163968da634d070fadee0ac372c84b244c0fc
Hans Georg Schaathun <georg@schaathun.net>
2025-10-03 13:48:16 +0200

    Added STACK and maxima

--- gitclone.sh
+++ gitclone.sh
@@ -2,5 +2,12 @@
 git clone -b MOODLE_500_STABLE https://github.com/moodle/moodle.git
 ( cd moodle/mod && git clone https://github.com/KQMATH/moodle-mod_capquiz.git capquiz )
 ( cd moodle/question/type && git clone https://github.com/trampgeek/moodle-qtype_coderunner.git coderunner )
+( cd moodle/question/type && git clone https://github.com/maths/moodle-qtype_stack.git stack )
+
+( cd moodle/question/behaviour/ && \
+  git clone https://github.com/maths/moodle-qbehaviour_dfexplicitvaildate.git dfexplicitvaildate && \
+  git clone https://github.com/maths/moodle-qbehaviour_dfcbmexplicitvaildate.git dfcbmexplicitvaildate && \
+  git clone https://github.com/maths/moodle-qbehaviour_adaptivemultipart.git adaptivemultipart )
+
 
 ( cd moodle/question/behaviour/ && git clone https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner.git adaptive_adapted_for_coderunner )
