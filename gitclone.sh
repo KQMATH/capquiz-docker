d39df66b6933b2bf5c131551f0e09b9080865f7a
Hans Georg Schaathun <georg@schaathun.net>
2025-10-03 15:11:17 +0200

    bugfixes in gitclone script

--- gitclone.sh
+++ gitclone.sh
@@ -1,5 +1,11 @@
 
+
 git clone -b MOODLE_500_STABLE https://github.com/moodle/moodle.git
+
+mkdir -p moodle/mod
+mkdir -p moodle/question/type
+mkdir -p moodle/question/behaviour
+
 ( cd moodle/mod && git clone https://github.com/KQMATH/moodle-mod_capquiz.git capquiz )
 ( cd moodle/question/type && git clone https://github.com/trampgeek/moodle-qtype_coderunner.git coderunner )
 ( cd moodle/question/type && git clone https://github.com/maths/moodle-qtype_stack.git stack )
